6bfefcd4f732e565b8af9fed011257d61739f87b
Merlin Volkmer <merlin.volkmer@outlook.com>
2025-11-12 15:33:49 +0100

    use headless firefox

--- composeApplication/build.gradle.kts
+++ composeApplication/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
         browser {
             testTask {
                 useKarma {
-                    useFirefox()
+                    useFirefoxHeadless()
                 }
             }
         }
